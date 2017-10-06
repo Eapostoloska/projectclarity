f7b3adefcda91cb9e0a1ab278e79e9e9023a09ab
Elena Apostoloska <Elena.Apostoloska@netcetera.com>
2017-10-07 01:09:03 +0200

    add node_modules

--- clarity-seed/src/app/app.module.ts
+++ clarity-seed/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { ROUTING } from "./app.routing";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
+import { TestComponentComponent } from './test-component/test-component.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         AboutComponent,
-        HomeComponent
+        HomeComponent,
+        TestComponentComponent
     ],
     imports: [
         BrowserAnimationsModule,
